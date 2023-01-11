6168dea083ab31ee5f2c473c4a1dca5045f3bad7
Cacheira14 <a038692@ismai.pt>
2023-01-11 23:53:18 +0000

    Correções finais.

--- Game/Assets/Scripts/UI Scripts/IntToText.cs
+++ Game/Assets/Scripts/UI Scripts/IntToText.cs
@@ -5,19 +5,27 @@ using UnityEngine.UI;
 
 public class IntToText : MonoBehaviour
 {
-
+    [SerializeField] private PlayerAdditional objectPA;
     [SerializeField] private int Value = 5;
     [SerializeField] private Text ValueText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Value = objectPA.hp;
     }
 
     // Update is called once per frame
     void Update()
     {
+        ChangeHpText();
         ValueText.text = Value.ToString();
         
+    }
+    public void ChangeHpText()
+    {
+        if (Value != objectPA.hp)
+        {
+            Value = objectPA.hp;
+        }
     }
 }
